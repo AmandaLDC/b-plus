b1214ba7e81550658d0efde70fb192824b935e0b
Amanda LDC <amanda.ldc@outlook.com>
2018-10-20 19:04:11 -0300

    Started implementing login page.

--- b-plus-client/src/app/login-page/login-page.component.ts
+++ b-plus-client/src/app/login-page/login-page.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
+
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  user : any;
+  model: any = {};
+
+  constructor(
+      private http: HttpClient,
+      private router: Router) {}
 
   ngOnInit() {
+    console.log("Page opens")
+  }
+
+  getPw(user){
+    var cpf = this.user.cpf;
+    var pw_aux = pw.split(".")
+    var pw = pw.append(pw_aux[0]);
+    pw = pw.append(pw_aux[1]);
+    pw = pw.join();
+    console.log(pw);
+  }
+
+  onSubmit() {
+      var m = JSON.stringify(this.model);
+      console.log(this.model)
+      this.http.get('http://dev2.unifacef.com.br:8000/api/matriculadoGrad/'+this.model.id).subscribe(dados => {
+        this.user = dados;
+      })
+      console.log(this.user)
+      var pw = this.getPw(this.user);
+      if(pw==this.model.password){
+        this.router.navigate(['/home-page']);
+      }
   }
 
 }
