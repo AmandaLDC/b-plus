db0fd8870e280eec5ff95bafede25260a9625035
Amanda LDC <amanda.ldc@outlook.com>
2018-10-20 20:49:14 -0300

    Succesfuly gets user input through form and ng-model.

--- b-plus-client/src/app/login-page/login-page.component.ts
+++ b-plus-client/src/app/login-page/login-page.component.ts
@@ -18,12 +18,12 @@ export class LoginPageComponent implements OnInit {
       private router: Router) {}
 
   ngOnInit() {
-    console.log("Page opens")
+    console.log("Page opens");
   }
 
   getPw(user){
     var cpf = this.user.cpf;
-    var pw_aux = pw.split(".")
+    var pw_aux = pw.split(".");
     var pw = pw.append(pw_aux[0]);
     pw = pw.append(pw_aux[1]);
     pw = pw.join();
@@ -32,11 +32,13 @@ export class LoginPageComponent implements OnInit {
 
   onSubmit() {
       var m = JSON.stringify(this.model);
-      console.log(this.model)
-      this.http.get('http://dev2.unifacef.com.br:8000/api/matriculadoGrad/'+this.model.id).subscribe(dados => {
+      console.log(this.model);
+      var id = JSON.stringify(this.model.id);
+      console.log(id);
+      this.http.get('http://dev2.unifacef.com.br:8000/api/matriculadoGrad/'+id).subscribe(dados => {
         this.user = dados;
       })
-      console.log(this.user)
+      console.log(this.user);
       var pw = this.getPw(this.user);
       if(pw==this.model.password){
         this.router.navigate(['/home-page']);
