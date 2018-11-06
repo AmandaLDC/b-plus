9a6b5898a679d2dd055289eec7cfe429d1a3de8d
Amanda LDC <amanda.ldc@outlook.com>
2018-11-06 21:11:44 -0200

    Directed search items to resulting pages.

--- b-plus-client/src/app/search-page/search-page.component.ts
+++ b-plus-client/src/app/search-page/search-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { SearchService } from '../search/search.service';
 
 @Component({
   selector: 'app-search-page',
@@ -11,12 +13,17 @@ export class SearchPageComponent implements OnInit {
   url : string = "http://dev2.unifacef.com.br:8000/api/exemplar"
   results : any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, public router: Router, private _searchService: SearchService) { }
 
   ngOnInit() {
     this.http.get(this.url).subscribe(data => {
       this.results = data;
     });
+  }
+
+  onClick(str : string){
+    this._searchService.save(str);
+    this.router.navigate(['/book-info']);
   }
 
 }
